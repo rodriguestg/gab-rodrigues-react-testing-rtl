9d5ccd7dec126f1d39d44e7517b7f9b66d18c1fc
rodriguestg <grs.ibr@gmail.com>
2022-07-18 18:50:58 -0300

    Subindo primeira parte req 1

--- src/tests/App.test.js
+++ src/tests/App.test.js
@@ -1,1 +1,20 @@
-test('', () => {});
+import React from 'react';
+import { screen, render } from '@testing-library/react';
+import { BrowserRouter } from 'react-router-dom';
+import App from '../App';
+
+describe('Testando o componente <App />', () => {
+  test('Testando se os links Home, About e Favorite estão no App', () => {
+    render(
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>,
+    );
+    const linkHome = screen.getByText(/Home/i);
+    expect(linkHome).toBeInTheDocument();
+    const linkAbout = screen.getByText(/About/i);
+    expect(linkAbout).toBeInTheDocument();
+    const linkFavoritePokemons = screen.getByText(/Favorite Pokémons/i);
+    expect(linkFavoritePokemons).toBeInTheDocument();
+  });
+});
