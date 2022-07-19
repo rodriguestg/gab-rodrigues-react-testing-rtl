1da5b4d9eb85eea032336db65f387dc13f7d8315
rodriguestg <grs.ibr@gmail.com>
2022-07-19 18:30:15 -0300

    Subindo req 4

--- src/tests/NotFound.test.js
+++ src/tests/NotFound.test.js
@@ -1,1 +1,16 @@
-test('', () => {});
+import React from 'react';
+import { screen, render } from '@testing-library/react';
+import { NotFound } from '../pages';
+
+describe('Testando o componente <NotFound />', () => {
+  test('Teste se a página contém um heading com uma mensagem', () => {
+    render(<NotFound />);
+    const h2 = screen.getByRole('heading');
+    expect(h2).toHaveTextContent(/Page requested not found 😭/i);
+  });
+  test('Teste se a página mostra a imagem', () => {
+    render(<NotFound />);
+    const img = screen.getAllByRole('img')[1];
+    expect(img).toHaveAttribute('src', 'https://media.giphy.com/media/kNSeTs31XBZ3G/giphy.gif');
+  });
+});
