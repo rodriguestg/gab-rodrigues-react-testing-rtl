6d4c53f78be1e7084e887a3946dce7e73441064b
rodriguestg <grs.ibr@gmail.com>
2022-07-20 23:11:45 -0300

    Subindo req 6 e correção lint

--- src/tests/Pokedex.test.js
+++ src/tests/Pokedex.test.js
@@ -1,7 +1,6 @@
 import React from 'react';
-import { screen, render } from '@testing-library/react';
+import { screen } from '@testing-library/react';
 import userEvent from '@testing-library/user-event';
-import { Pokedex } from '../pages';
 import renderWithRouter from '../renderWithRouter';
 import App from '../App';
 
--- src/tests/Pokemon.test.js
+++ src/tests/Pokemon.test.js
@@ -1,1 +1,34 @@
-test('', () => {});
+import React from 'react';
+import { screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+import App from '../App';
+import renderWithRouter from '../renderWithRouter';
+
+describe('Testando o componente <Pokemon />', () => {
+  test('Teste se a página contém o nome correto do pokemon', () => {
+    renderWithRouter(<App />);
+    const namePoke = screen.getByTestId('pokemon-name');
+    expect(namePoke).toHaveTextContent(/Pikachu/i);
+  });
+  test('Teste se a página contém o tipo correto do pokemon', () => {
+    renderWithRouter(<App />);
+    const namePoke = screen.getByTestId('pokemon-type');
+    expect(namePoke).toHaveTextContent(/Electric/i);
+  });
+  test('Teste se a página contém a descrição (alt) e icon correto', () => {
+    renderWithRouter(<App />);
+    const favLink = screen.getByRole('link', { name: 'More details' });
+    userEvent.click(favLink);
+    const favCheck = screen.getByLabelText('Pokémon favoritado?');
+    userEvent.click(favCheck);
+    const fav = screen.getAllByRole('img')[1];
+    expect(fav).toHaveAttribute('alt', 'Pikachu is marked as favorite');
+    expect(fav).toHaveAttribute('src', '/star-icon.svg');
+  });
+  test('Teste se a página contém a imagem do pokemon com alt e icon correto', () => {
+    renderWithRouter(<App />);
+    const fav = screen.getAllByRole('img')[0];
+    expect(fav).toHaveAttribute('alt', 'Pikachu sprite');
+    expect(fav).toHaveAttribute('src', 'https://cdn2.bulbagarden.net/upload/b/b2/Spr_5b_025_m.png');
+  });
+});
